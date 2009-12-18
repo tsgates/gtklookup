d6dadcb411d2074b481b6c5a5af79eea3186c428
Taesoo <taesoo@mit.edu>
2009-12-18 17:50:25 -0500

    gtk+ reference lookup

---
+++ gtklookup.el
@@ -0,0 +1,102 @@
+(eval-when-compile
+  (require 'browse-url)
+  (require 'simple)
+  (require 'cl)
+  (require 'ido)
+  (require 'anything))
+
+;;=================================================================
+;; user options
+;;=================================================================
+
+(defvar gtklookup-db-file "gtklookup.db" "gtklookup database file")
+(defvar gtklookup-program "gtklookup.py" "gtklookup execution file")
+
+(defvar gtklookup-all   nil)
+(defvar gtklookup-cache nil)
+
+;;=================================================================
+;; execute gtklookup
+;;=================================================================
+
+(defun gtklookup-exec-get-cache ()
+  "Run a gtklookup process and get a list of cache (db key)"
+
+  (split-string
+   (with-output-to-string
+     (call-process gtklookup-program nil standard-output nil 
+           "-d" (expand-file-name gtklookup-db-file)
+           "-c"))))
+
+(defun gtklookup-exec-lookup (search-term)
+  "Runs a gtklookup process and returns a list of (term, url) pairs."
+
+  (mapcar 
+   (lambda (x) (split-string x "\t"))
+   (split-string
+     (with-output-to-string
+         (call-process gtklookup-program nil standard-output nil 
+                       "-d" (expand-file-name gtklookup-db-file) 
+               "-l" search-term))
+     "\n" t)))
+
+(defun gtklookup-exec-all ()
+  "Runs a gtklookup process and returns a list of all candidates"
+
+  ;; (key, url, cat)
+  (mapcar 
+   (lambda (x) (split-string x "\t"))
+   (split-string
+     (with-output-to-string
+         (call-process gtklookup-program nil standard-output nil 
+                       "-d" (expand-file-name gtklookup-db-file) 
+                       "-a"))
+     "\n" t)))
+
+;;=================================================================
+;; interactive user interfaces
+;;=================================================================
+
+(defun gtklookup-get-candidate ()
+  "Return ( display, url ) pairs"
+
+  (if gtklookup-all
+      gtklookup-all
+    (setq gtklookup-all 
+          (mapcar (lambda (x) (cons (format "%-40s : %5s" (car x) (caddr x)) (cadr x)))
+                  (gtklookup-exec-all)))))
+
+(defun gtklookup-get-cache ()
+  "Fetching cache from gtklookup.py"
+
+  (if gtklookup-cache 
+      gtklookup-cache 
+    (setq gtklookup-cache (gtklookup-exec-get-cache))))
+
+(defvar anything-c-source-gtk
+  '((name . "Gtk+")
+    (candidates . gtklookup-get-candidate)
+    (action . browse-url))
+  "See GTK+ Reference")
+
+;; (anything 'anything-c-source-gtk)
+
+;;;###autoload
+(defun gtklookup-lookup ()
+  "Lookup SEARCH-TERM in the GTK+ Reference indexes."
+  
+  (interactive)
+  (anything 'anything-c-source-gtk))
+
+;;;###autoload
+(defun gtklookup-update (src)
+  "Run gtklookup-update and create the database at `gtklookup-db-file'."
+  (interactive)
+
+  ;; gtklookup.py -d /home/taesoo/.gtklookup/gtklookup.db -u
+  (call-process gtklookup-program nil standard-output nil 
+                "-d" (expand-file-name gtklookup-db-file) 
+                "-u"))
+
+(provide 'gtklookup)
+;;; gtklookup.el ends here
---
+++ gtklookup.py
@@ -0,0 +1,137 @@
+#! /usr/bin/env python
+
+"""
+Taesoo Kim (tsgatesv@gmail.com)
+
+gtklookup is to lookup entries from gtk reference documentation, especially
+within emacs. (http://www.gtk.org/documentation.html)
+
+Have a fun!
+"""
+
+import pickle
+
+from urllib        import urlopen
+from os.path       import join, dirname, exists
+
+debug = False
+
+def fetch_page( db, root, cat ) :
+    from BeautifulSoup import BeautifulSoup
+
+    pairs = [ ( "ix01.html", "indexdiv" ),
+              ( "api-index-full.html", "index" ),
+              ( "index-all.html", "indexdiv" ) ]
+
+    for ( index, div ) in pairs :
+        try :
+            main = BeautifulSoup( urlopen( join( root, index )  ) )
+            body = main.findAll( "div", attrs={ "class" : div } )[0]
+            links = body.findAll( "a" )
+            break
+        
+        except :
+            if debug :
+                print "Skipped! (%s,%s)" % ( index, div )
+
+    for link in links :
+        try :
+            url = link[ "href" ].strip()
+        except :
+            continue
+
+        desc = link.string
+        desc = desc.replace( "()", "" )
+        desc = desc.replace( " ", "" ).strip()
+
+        url = join( root, url.strip() )
+        
+        db.append( ( desc.encode( "ascii", "ignore" ), 
+                     url.encode( "ascii", "ignore" ),
+                     cat.encode( "ascii", "ignore" ) ) )
+
+        if debug :
+            print "%-40s(%s):%s" % ( desc, cat, url )
+
+def update_db( dbname ) :
+    # clean up
+    db = []
+    
+    # ref. http://www.gtk.org/documentation.html
+    urls = { "GLib"      : "http://library.gnome.org/devel/glib/stable/",
+             "GObject"   : "http://library.gnome.org/devel/gobject/stable/",
+             "GIO"       : "http://library.gnome.org/devel/gio/stable/",
+             "Pango"     : "http://library.gnome.org/devel/pango/stable/",
+             "ATK"       : "http://library.gnome.org/devel/atk/stable/",
+             "GdkPixbuf" : "http://library.gnome.org/devel/gdk-pixbuf/stable/",
+             "GDK"       : "http://library.gnome.org/devel/gdk/stable/",
+             "GTK"       : "http://library.gnome.org/devel/gtk/stable/" }
+
+    root = urls[ "GLib" ]
+
+    for (cat, url) in urls.iteritems() :
+        print "Fetching %-10s : %s" % ( cat, url )
+        
+        fetch_page( db, url, cat )
+
+    # sort db
+    db.sort()
+    
+    # update database
+    pickle.dump( db, open( dbname, "w" ) )
+
+if __name__ == "__main__" :
+    import optparse
+    
+    parser = optparse.OptionParser( __doc__.strip() )
+    
+    parser.add_option( "-d", "--db"       , dest="db", default="gtklookup.db" )
+    parser.add_option( "-b", "--debug"    , action="store_true", dest="debug", default=False )
+    parser.add_option( "-l", "--lookup"   , dest="key" )
+    parser.add_option( "-u", "--update"   , action="store_true", dest="update", default=False )
+    parser.add_option( "-a", "--all"      , action="store_true", default=False, dest="all")
+    parser.add_option( "-c", "--cache"    , action="store_true", default=False, dest="cache")
+
+    ( opts, args ) = parser.parse_args()
+
+    debug = opts.debug
+    
+    # create db
+    try:
+        if exists( opts.db ) :
+            db = pickle.load( open( opts.db ) )
+        else :
+            db = []
+    except :
+        db = []
+
+    # update
+    if opts.update :
+        update_db( opts.db )
+        exit
+        
+    # cache
+    if opts.cache :
+        print "\n".join( [ item[0] for item in db ] )
+        exit
+        
+    # complete
+    if opts.all :
+        print "\n".join( [ "\t".join( item ) for item in db ] )
+        exit
+        
+    # lookup
+    if opts.key :
+        
+        results = []
+        
+        for item in db :
+            if any( [ subitem.find( opts.key ) != -1 for subitem in item ] ) :
+                results.append( item )
+        
+        results.sort()
+
+        print "\n".join( [ "\t".join( item ) for item in results ] )
+        exit
+
+                    
