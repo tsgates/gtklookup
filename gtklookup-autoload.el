c1952be6c3f7af6a879d30908b57d9694318aae9
Taesoo <taesoo@mit.edu>
2009-12-18 18:04:41 -0500

    add README & autoloader

---
+++ gtklookup-autoload.el
@@ -0,0 +1,20 @@
+;; 
+;; gtk autoloader
+;;
+
+(eval-when-compile (require 'gtklookup))
+
+(defvar gtklookup-dir ".")
+
+;; set executable file and db file
+(setq gtklookup-program (concat gtklookup-dir "/gtklookup.py"))
+(setq gtklookup-db-file (concat gtklookup-dir "/gtklookup.db"))
+
+;; to speedup, just load it on demand
+(autoload 'gtklookup-lookup "gtklookup"
+  "Lookup SEARCH-TERM in the GTK+ HTML indexes." t)
+
+(autoload 'gtklookup-update "gtklookup" 
+  "Run gtklookup-update and create the database at `gtklookup-db-file'." t)
+
+(provide 'gtklookup-autoload)
